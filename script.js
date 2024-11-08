783007d6a4f9f2c861a11662dac7bcf0651a76ad
NicoMaker <Nicola.marano02gmail.com>
2024-11-08 12:01:17 +0100

    modifica con arrow function

--- script.js
+++ script.js
@@ -4,46 +4,43 @@ function contactEmail(nome, cognome) {
   window.location.href = `mailto:${email}?subject=Contact`;
 }
 
-function openPDF() {
-  window.open("https://example.com/your-pdf-link", "_blank");
-}
-
-function openGit() {
-  window.open(
-    "https://github.com/CammarataLudovico/Front-End_2024-2026",
-    "_blank"
-  );
-}
+const openPDF = () =>
+    window.open("https://example.com/your-pdf-link", "_blank"),
+  openGit = () =>
+    window.open(
+      "https://github.com/CammarataLudovico/Front-End_2024-2026",
+      "_blank"
+    );
 
 // Caricamento dinamico dei dati e generazione delle card
 document.addEventListener("DOMContentLoaded", async function () {
   const cardContainer = document.getElementById("card-container");
 
   try {
-    const response = await fetch("Studenti.json");
-    const cardData = await response.json();
+    const response = await fetch("Studenti.json"),
+      cardData = await response.json();
 
     cardData.forEach((data) => {
       // Creazione della card
-      const card = document.createElement("div");
-      card.className = "col-12 col-sm-6 col-md-4 col-lg-3 col-custom-5";
+      const card = document.createElement("div"),
+        cardInner = document.createElement("div"),
+        img = document.createElement("img"),
+        numberDiv = document.createElement("div"),
+        nameDiv = document.createElement("div"),
+        button = document.createElement("button");
 
-      const cardInner = document.createElement("div");
+      card.className = "col-12 col-sm-6 col-md-4 col-lg-3 col-custom-5";
       cardInner.className = "card p-3 shadow-sm";
 
-      const img = document.createElement("img");
       img.src = data.immagine;
       img.alt = `Image ${data.numero}`;
 
-      const numberDiv = document.createElement("div");
       numberDiv.className = "number";
       numberDiv.textContent = `${data.numero}.`;
 
-      const nameDiv = document.createElement("div");
       nameDiv.className = "name";
       nameDiv.textContent = `${data.nome} ${data.cognome}`;
 
-      const button = document.createElement("button");
       button.className = "btn btn-primary contact-btn";
       button.textContent = "Invia Mail";
       button.onclick = () => contactEmail(data.nome, data.cognome);
